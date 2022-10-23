1807aa034b49ea50cb5898956445626aae0db9fe
umeshawijesingha <umeshawijesingha@gmail.com>
2022-10-23 22:29:10 +0530

    Contact us page created

---
+++ src/Pages/Contact.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../Components/Header/Header'
+import Navbar from '../Components/Navbar/Navbar'
+import ContactUs from '../Components/ContactUs/ContactUs'
+import Footer from '../Components/Footer/Footer'
+
+const Contact = () => {
+  return (
+    <div>
+        <Header/>
+      <Navbar/>
+      <ContactUs/>
+      <Footer/>
+        </div>
+  )
+}
+
+export default Contact
